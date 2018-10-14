3db15c651462f34e4b74bfe0da1c379ca7a83190
griffith <griffithawuah15@gmail.com>
2018-10-14 13:40:57 +0000

    improved router

--- src/router.js
+++ src/router.js
@@ -3,8 +3,9 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Authenticate from './views/Authenticate.vue'
 import ManagerApplication from './views/ManagerApplication.vue'
-
-
+import ManagerHome from './components/subviews/ManagerHome.vue'
+import ManagerSettings from './components/subviews/ManagerSettings.vue'
+import Listing from './components/subviews/Listing.vue'
 
 Vue.use(Router)
 
@@ -28,7 +29,24 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: ManagerApplication
+      component: ManagerApplication,
+      children:[
+        {
+          path: 'home',
+          name: 'Home',
+          component: ManagerHome
+        },
+        {
+          path: 'settings',
+          name: 'Settings',
+          component: ManagerSettings
+        },
+        {
+          path: 'listing',
+          name: 'Listing',
+          component: Listing
+        }
+      ]
     }
   ]
 })
